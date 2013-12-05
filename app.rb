6e0daade45140a4b7d7b007578070eae7ae235d8
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2013-12-05 17:06:17 +0100

    Refactor courses.json

--- app.rb
+++ app.rb
@@ -2,12 +2,35 @@ require 'sinatra'
 
 require File.join(File.dirname(__FILE__), 'config', 'yummly.rb') unless ENV['RACK_ENV'] == 'production'
 
-get '/courses' do
-  uri = URI('http://api.yummly.com/v1/api/metadata/course')
-  params = { _app_id: ENV['YUMMLY_APP_ID'], _app_key: ENV['YUMMLY_APP_KEY'] }
-  uri.query = URI.encode_www_form(params)
+def base_uri
+  'http://api.yummly.com/v1/api/'
+end
 
+def authorization_params
+  { _app_id: ENV['YUMMLY_APP_ID'], _app_key: ENV['YUMMLY_APP_KEY'] }
+end
+
+def generate_uri_for(endpoint)
+  uri = URI("#{base_uri}#{endpoint}")
+  uri.query = URI.encode_www_form(authorization_params)
+  uri
+end
+
+def clean_json_response(response, resource)
+  response.gsub(/set_metadata\(\'#{resource}\'\,/, '').gsub(/\);$/, '')
+end
+
+def cache_data(filename, data)
+  File.open(File.join(File.dirname(__FILE__), 'public', filename), "w") do |cache_file|
+    cache_file.write data
+  end
+end
+
+get '/courses' do
+  uri = generate_uri_for('metadata/course')
   result = Net::HTTP.get_response(uri)
-  courses = result.body.gsub(/set_metadata\(\'course\'\,/, '').gsub(/\);$/, '')
-  "{ \"courses\": #{courses} }"
+  courses = clean_json_response(result.body, 'course')
+  courses = "{ \"courses\": #{courses} }"
+  cache_data('courses.json', courses)
+  courses
 end
--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -1,10 +1,18 @@
 require 'spec_helper'
 
 describe "Courses" do
-  before { visit '/courses' }
 
   it "returns a list of courses" do
+    visit '/courses'
     data = JSON.parse(page.body)
     expect(data['courses'].length).to be > 0
+  end
+
+  it "caches the courses" do
+    cache_file = File.join(File.dirname(__FILE__), '..', '..', 'public', 'courses.json')
+    File.unlink(cache_file) if File.exists?(cache_file)
+    visit '/courses'
+
+    expect(File.exists?(cache_file)).to be true
   end
 end
